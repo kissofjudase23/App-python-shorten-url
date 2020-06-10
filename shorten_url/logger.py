e330672b866218db927a2fc9c8d04a2923d99731
Tom Lin <tomlin@synology.com>
2020-06-10 21:35:50 +0800

    Add variables and logger

---
+++ shorten_url/logger.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import logging.handlers
+from enum import IntEnum, unique
+
+
+@unique
+class LogMode(IntEnum):
+    CONSOLE = 1
+    SYSLOG = 2
+
+
+class LogFactory(object):
+    _SYSLOG_SOCKET = "/dev/log"
+    # Ref: https://docs.python.org/3/library/logging.html#logrecord-attributes
+    _FORMATTER = logging.Formatter("[%(asctime)s] [%(levelname)s] [%(name)s] "
+                                   "[%(filename)s:%(funcName)s:%(lineno)d] "
+                                   "[%(message)s]")
+
+    _JSON_FORMATTER = logging.Formatter(
+        "{'time':'%(asctime)s', 'level':'%(levelname)s', 'logger_name':'%(name)s', "
+        "'file':'%(filename)s:%(funcName)s:%(lineno)d', "
+        "'message': '%(message)s'}")
+
+    @classmethod
+    def logger(cls,
+               *,
+               name,
+               mode=LogMode.CONSOLE,
+               log_level=logging.INFO,
+               ignore_process_thread=True,
+               ignore_src_file=False,
+               suppress_raise=True):
+
+        """ custom logger
+            Args:
+                name:
+                    name of your logger, use __name__ as a module level logger
+                mode:
+                    please refer LogMode
+                log_level:
+                    log level for your logger and handlers
+                    ex: logging.INFO, logging.DEBUG
+                suppress_raise:
+                    Swallow exceptions while logging, suggest to use this option
+                    on prod env
+            Returns:
+                logger obj
+        """
+        if ignore_process_thread:
+            logging.logThreads = 0
+            logging.logProcesses = 0
+
+        if ignore_src_file:
+            # file name/ function/ line number will be None
+            logging._srcfile = None
+
+        if suppress_raise:
+            logging.raiseExceptions = False
+
+        logger = logging.getLogger(name)
+        logger.setLevel(log_level)
+
+        # handler log level are different from logger level
+        if mode & LogMode.CONSOLE:
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(log_level)
+            console_handler.setFormatter(cls._JSON_FORMATTER)
+            logger.addHandler(console_handler)
+
+        if mode & LogMode.SYSLOG:
+            sys_handler = logging.handlers.SysLogHandler(address=cls._SYSLOG_SOCKET)
+            sys_handler.setLevel(log_level)
+            sys_handler.setFormatter(cls._JSON_FORMATTER)
+            logger.addHandler(sys_handler)
+
+        return logger
---
+++ shorten_url/variables.py
@@ -0,0 +1,53 @@
+
+from enum import Enum
+from distutils.util import strtobool
+import os
+from pprint import pprint as pp
+from shorten_url import exc
+
+
+class DbTypes(Enum):
+    MOCK = "mock"
+    MYSQL = "mysql"
+
+
+class CacheTypes(Enum):
+    MOCK = "mock"
+    REDIS = "redis"
+
+
+SUPPORTED_DB_TYPES = tuple(enum.value for enum in DbTypes)
+SUPPORTED_CACHE_TYPES = tuple(enum.value for enum in CacheTypes)
+
+
+def get_os_env(key, check_list=None):
+    if key not in os.environ:
+        raise exc.EnvVariableMissing(f"cat not find {key} in os.env")
+
+    val = os.environ[key]
+
+    if check_list and val not in check_list:
+        raise exc.NotSupportedEnvVariable(f"do not support {val} for {key}")
+
+    return os.environ[key]
+
+
+class AppVars(object):
+    ENABLE_SWAGGER = strtobool(get_os_env(key="APP_ENABLE_SWAGGER"))
+
+
+class DbVars(object):
+    TYPE = get_os_env(key="DB_TYPE", check_list=SUPPORTED_DB_TYPES)
+    HOST = get_os_env("DB_HOST")
+    PORT = int(get_os_env("DB_PORT"))
+    MAX_CONNECTIONS = int(get_os_env("DB_MAX_CONNECTIONS"))
+    DATABASE = get_os_env("DB_DATABASE")
+    USER = get_os_env("DB_USER")
+    PASSWORD = get_os_env("DB_PASSWORD")
+
+
+class CacheVars(object):
+    TYPE = get_os_env(key="CACHE_TYPE", check_list=SUPPORTED_CACHE_TYPES)
+    HOST = get_os_env("CACHE_HOST")
+    PORT = int(get_os_env("CACHE_PORT"))
+    MAX_CONNECTIONS = int(get_os_env("CACHE_MAX_CONNECTIONS"))
