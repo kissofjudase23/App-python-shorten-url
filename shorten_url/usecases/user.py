57300f84b0c444cf794c0d5f26a81502ed9a1365
Tom Lin <tomlin@synology.com>
2020-06-26 18:59:11 +0800

    Update user use cases
    Use property to set/get cache and user_repo

--- shorten_url/usecases/user.py
+++ shorten_url/usecases/user.py
@@ -16,11 +16,27 @@ def marshal_user_entities(user_entitries: List[UserEntitry]) -> List[dict]:
 class User(metaclass=Singleton):
 
     def __init__(self,
-                 user_repo: UserRepositoryABC,
-                 cache: CacheABC):
+                 user_repo: UserRepositoryABC = None,
+                 cache: CacheABC = None):
 
-        self.user_repo = user_repo
-        self.cache = cache
+        self._user_repo = user_repo
+        self._cache = cache
+
+    @property
+    def user_repo(self) -> UserRepositoryABC:
+        return self._user_repo
+
+    @user_repo.setter
+    def user_repo(self, user_repo: UserRepositoryABC):
+        self._user_repo = user_repo
+
+    @property
+    def cache(self) -> CacheABC:
+        return self._cache
+
+    @cache.setter
+    def cache(self, cache: CacheABC):
+        self._cache = cache
 
     def add_user(self, name, email):
         """ add a new user
@@ -30,7 +46,7 @@ class User(metaclass=Singleton):
         Raise :
             DuplicateUserError
         """
-        return self.user_repo.add_user(name, email)
+        return self._user_repo.add_user(name, email)
 
     def list_users(self, page=0, page_size=100) -> List[dict]:
         """ List the users
@@ -41,22 +57,22 @@ class User(metaclass=Singleton):
             List[UserEntitry]
         """
         cache_key = f"User:list_users:{page}:{page_size}"
-        cache_result = self.cache.get_json(cache_key)
+        cache_result = self._cache.get_json(cache_key)
         if cache_result:
             return cache_result
 
-        user_entities = self.user_repo.list_users(page, page_size)
+        user_entities = self._user_repo.list_users(page, page_size)
         marshalled_result = marshal_user_entities(user_entities)
 
         # marshal_user_entities
         cache_ex = 10
-        self.cache.set_json(key=cache_key,
-                            val=marshalled_result,
-                            ex=cache_ex)
+        self._cache.set_json(key=cache_key,
+                             val=marshalled_result,
+                             ex=cache_ex)
         return marshalled_result
 
     def delete_user(self, user_id):
-        return self.user_repo.delete_user(user_id)
+        return self._user_repo.delete_user(user_id)
 
     def get_user_info(self, user_id) -> dict:
         """ Get the user information
@@ -65,13 +81,14 @@ class User(metaclass=Singleton):
             NoUserFoundError
         """
         cache_key = f"User:get_user_info:{user_id}"
-        cache_result = self.cache.get_json(cache_key)
+        cache_result = self._cache.get_json(cache_key)
         if cache_result:
             return cache_result
 
-        user_entity = self.user_repo.get_user_info(user_id)
+        user_entity = self._user_repo.get_user_info(user_id)
         cache_ex = 10
-        self.cache.set_json(key=cache_key,
-                            val=user_entity.asdict,
-                            ex=cache_ex)
+        self._cache.set_json(key=cache_key,
+                             val=user_entity.asdict,
+                             ex=cache_ex)
+
         return user_entity.asdict
