5f4db5cf4a3647580de79dbd38d93fef2123b2c5
Tom Lin <tomlin@synology.com>
2020-06-27 17:14:34 +0800

    Add list_users test case for user_usecases

--- shorten_url/usecases/test_user_usecase.py
+++ shorten_url/usecases/test_user_usecase.py
@@ -1,4 +1,5 @@
-from shorten_url.variables import DbTypes
+from typing import List
+
 from shorten_url.models.user import UserEntitry, UserRepositoryABC
 from shorten_url.cache import CacheABC
 from shorten_url.usecases.user import User
@@ -17,6 +18,12 @@ test_user_ids = [
     ("55555"),
     ("999999999"),
     ("1000000000000000000000000000000000")
+]
+
+test_user_entities = [
+    ([UserEntitry(name="Tom", email="Tom@gmail.com", id="1"),
+      UserEntitry(name="Emily", email="Emily@gmail.com", id="2"),
+      UserEntitry(name="Jason", email="Jason@gmail.com", id="3")])
 ]
 
 
@@ -50,9 +57,9 @@ class TestUserUseCases(object):
         mock_user_repo.get_user_info.assert_not_called()
 
     @pytest.mark.parametrize("user_id", test_user_ids)
-    def test_get_user_info_do_not_use_cache(self,
-                                            user_usecases: User,
-                                            user_id):
+    def test_get_user_info_(self,
+                            user_usecases: User,
+                            user_id):
 
         # create and set the mock objects
         mock_user_repo = mock.create_autospec(UserRepositoryABC)
@@ -72,6 +79,70 @@ class TestUserUseCases(object):
 
         # check assert calls
         mock_user_repo.get_user_info.assert_called_once_with(user_id)
-        mock_cache.set_json.assert_called_once_with(key=user_usecases.get_user_info_key(user_id),
+
+        cache_key = user_usecases.get_user_info_key(user_id)
+        mock_cache.get_json.assert_called_once_with(key=cache_key)
+        mock_cache.set_json.assert_called_once_with(key=cache_key,
                                                     val=user_entity.asdict,
                                                     ex=user_usecases.CACHE_EX_GET_USER_INFO)
+
+
+    @pytest.mark.parametrize("user_entities", test_user_entities)
+    def test_list_users_use_cache(self,
+                                  user_usecases: User,
+                                  user_entities: List[UserEntitry],
+                                  page=0,
+                                  page_size=100):
+
+        # create and set the mock objects
+        mock_user_repo = mock.create_autospec(UserRepositoryABC)
+        mock_cache = mock.create_autospec(CacheABC)
+        user_usecases.user_repo = mock_user_repo
+        user_usecases.cache = mock_cache
+
+        # mock the return value
+        expected = [e.asdict for e in user_entities]
+        mock_cache.get_json.return_value = expected
+
+        # check results
+        actual = user_usecases.list_users(page, page_size)
+        assert actual == expected
+
+        # check assert calls
+        cache_key = User.get_list_users_key(page, page_size)
+        mock_cache.get_json.assert_called_once_with(key=cache_key)
+
+        mock_cache.set_json.assert_not_called()
+        mock_user_repo.list_users.assert_not_called()
+
+
+    @pytest.mark.parametrize("user_entities", test_user_entities)
+    def test_list_users(self,
+                        user_usecases: User,
+                        user_entities: List[UserEntitry],
+                        page=0,
+                        page_size=100):
+
+        # create and set the mock objects
+        mock_user_repo = mock.create_autospec(UserRepositoryABC)
+        mock_cache = mock.create_autospec(CacheABC)
+        user_usecases.user_repo = mock_user_repo
+        user_usecases.cache = mock_cache
+
+        # mock the return value
+        mock_cache.get_json.return_value = None
+        mock_user_repo.list_users.return_value = user_entities
+
+        # check results
+        expected = [e.asdict for e in user_entities]
+        actual = user_usecases.list_users(page, page_size)
+        assert actual == expected
+
+        # check assert calls
+        mock_user_repo.list_users.assert_called_once_with(page, page_size)
+
+        cache_key = User.get_list_users_key(page, page_size)
+        mock_cache.get_json.assert_called_once_with(key=cache_key)
+        mock_cache.set_json.assert_called_once_with(key=cache_key,
+                                                    val=expected,
+                                                    ex=User.CACHE_EX_LIST_USERS)
